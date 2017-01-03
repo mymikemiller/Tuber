0b90863916efcbdfcafe2187c62acb418ae0b9ac
phistrix <phistrix@gmail.com>
2017-01-02 23:22:31 -0700

    Added menu item allowing the user to choose which google account to use

--- app/src/main/java/com/phistrix/tuber/MainActivity.java
+++ app/src/main/java/com/phistrix/tuber/MainActivity.java
@@ -1,13 +1,102 @@
 package com.phistrix.tuber;
 
+import android.accounts.AccountManager;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.common.collect.Lists;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String ACCOUNT_KEY = "accountName";
+    private static final int REQUEST_ACCOUNT_PICKER = 0;
+
+    GoogleAccountCredential mCredential;
+    private String mChosenAccountName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            mChosenAccountName = savedInstanceState.getString(ACCOUNT_KEY);
+        } else {
+            loadAccount();
+        }
+
+        mCredential =
+                GoogleAccountCredential.usingOAuth2(getApplicationContext(), Lists.newArrayList(Auth.SCOPES));
+        mCredential.setSelectedAccountName(mChosenAccountName);
+        mCredential.setBackOff(new ExponentialBackOff());
     }
+
+    private void chooseAccount() {
+        startActivityForResult(mCredential.newChooseAccountIntent(),
+                REQUEST_ACCOUNT_PICKER);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_ACCOUNT_PICKER:
+                if (resultCode == Activity.RESULT_OK && data != null
+                        && data.getExtras() != null) {
+                    String accountName = data.getExtras().getString(
+                            AccountManager.KEY_ACCOUNT_NAME);
+                    if (accountName != null) {
+                        mChosenAccountName = accountName;
+                        mCredential.setSelectedAccountName(accountName);
+                        saveAccount();
+                    }
+                }
+                break;
+        }
+    }
+
+    private void loadAccount() {
+        SharedPreferences sp = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        mChosenAccountName = sp.getString(ACCOUNT_KEY, null);
+        invalidateOptionsMenu();
+    }
+
+    private void saveAccount() {
+        SharedPreferences sp = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        sp.edit().putString(ACCOUNT_KEY, mChosenAccountName).commit();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(ACCOUNT_KEY, mChosenAccountName);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_accounts:
+                chooseAccount();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
